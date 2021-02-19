4b158a913c73acda06af8f8fed201197b44cc93e
Bratescu <abratescu@deloittece.com>
2021-02-19 15:00:57 +0200

    new update

--- script.js
+++ script.js
@@ -6,14 +6,30 @@ $(document).ready(function () {
   $(".csr-contact-email").mouseout(function () {
     $(this).parent().parent().find(".csr-contact-image-wrapper").removeClass("hovered");
   });
-});
+  // SCROLL STUFF
+  const sections = [...document.querySelectorAll(".scroll-item")];
 
-$(".mouse").click(function () {
-  console.log("clicked");
-  $([document.documentElement, document.body]).animate(
-    {
-      scrollTop: $("#csr-mid").offset().top - 80,
-    },
-    800
-  );
+  let options = {
+    rootMargin: "0px",
+    threshold: 0.25,
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      const { target } = entry;
+      console.log(entry, target);
+
+      if (entry.intersectionRatio >= 0.25) {
+        target.classList.add("is-visible");
+      } else {
+        target.classList.remove("is-visible");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  sections.forEach((section, index) => {
+    observer.observe(section);
+  });
 });
