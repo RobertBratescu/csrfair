bb1844de37c08ec111d209ea37d4d18b03be1c85
Robert <robert.bratescu29@gmail.com>
2021-02-24 09:56:12 +0200

    more changes

--- script.js
+++ script.js
@@ -1,87 +1,89 @@
 $(document).ready(function () {
-  console.log("ok");
-  $(".csr-contact-email").mouseover(function () {
-    $(this).parent().parent().find(".csr-contact-image-wrapper").addClass("hovered");
-  });
-  $(".csr-contact-email").mouseout(function () {
-    $(this).parent().parent().find(".csr-contact-image-wrapper").removeClass("hovered");
-  });
-  // SCROLL STUFF
-  const sections = [...document.querySelectorAll(".scroll-item")];
+	console.log('ok');
+	$('.csr-contact-email').mouseover(function () {
+		$(this).parent().parent().find('.csr-contact-image-wrapper').addClass('hovered');
+	});
+	$('.csr-contact-email').mouseout(function () {
+		$(this).parent().parent().find('.csr-contact-image-wrapper').removeClass('hovered');
+	});
+	// SCROLL STUFF
+	const sections = [...document.querySelectorAll('.scroll-item')];
 
-  let options = {
-    rootMargin: "0px",
-    threshold: 0.25,
-  };
+	let options = {
+		rootMargin: '0px',
+		threshold: 0.25,
+	};
 
-  const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      const { target } = entry;
-      console.log(entry, target);
+	const callback = (entries, observer) => {
+		entries.forEach((entry) => {
+			const { target } = entry;
+			console.log(entry, target);
 
-      if (entry.intersectionRatio >= 0.25) {
-        target.classList.add("is-visible");
-      } else {
-        // target.classList.remove("is-visible");
-      }
-    });
-  };
+			if (entry.intersectionRatio >= 0.25) {
+				target.classList.add('is-visible');
+			} else {
+				// target.classList.remove("is-visible");
+			}
+		});
+	};
 
-  const observer = new IntersectionObserver(callback, options);
+	const observer = new IntersectionObserver(callback, options);
 
-  sections.forEach((section, index) => {
-    observer.observe(section);
-  });
+	sections.forEach((section, index) => {
+		observer.observe(section);
+	});
 
-  //CLICK LEARN MORE
-  $(".csr-card-button").click(function () {
-    $(".card").removeClass("open");
-    $(".card").removeClass("lower");
-    $(this).parent().parent().addClass("open");
-    $(".card:not(.open)").addClass("lower");
+	//CLICK LEARN MORE
+	$('.csr-card-button').click(function () {
+		$('.card').removeClass('open');
+		$('.card').removeClass('lower');
+		$(this).parent().parent().addClass('open');
+		$('.card:not(.open)').addClass('lower');
 
-    $(".csr-intro-title, .csr-intro-description").addClass("go-up");
+		$('.csr-intro-title, .csr-intro-description').addClass('go-up');
 
-    $([document.documentElement, document.body]).animate(
-      {
-        scrollTop: $(this).parent().parent().offset().top - 30,
-      },
-      900
-    );
-  });
+		$('.csr-cards-wrapper').addClass('go-up');
 
-  //   CLICK A PROJECT
-  $(".csr-project").click(function () {
-    $(".csr-intro").addClass("has-left");
-    // Get the data
-    var getData = $(this).attr("data-tab");
-    $(".project-page").addClass("hide");
-    $(".project-page[data-tab='" + getData + "']").removeClass("hide");
+		$([document.documentElement, document.body]).animate(
+			{
+				scrollTop: $(this).parent().parent().offset().top - 30,
+			},
+			900
+		);
+	});
 
-    $(".csr-projects-section").addClass("has-left");
+	//   CLICK A PROJECT
+	$('.csr-project').click(function () {
+		$('.csr-intro').addClass('has-left');
+		// Get the data
+		var getData = $(this).attr('data-tab');
+		$('.project-page').addClass('hide');
+		$(".project-page[data-tab='" + getData + "']").removeClass('hide');
 
-    $(".card").removeClass("open");
+		$('.csr-projects-section').addClass('has-left');
 
-    $([document.documentElement, document.body]).animate(
-      {
-        scrollTop: $(".csr-mid").offset().top - 30,
-      },
-      600
-    );
+		$('.card').removeClass('open');
 
-    $(".return-button").removeClass("go-down");
-  });
+		$([document.documentElement, document.body]).animate(
+			{
+				scrollTop: $('.csr-mid').offset().top - 30,
+			},
+			600
+		);
 
-  //   CLICK RETURN
-  $(".return-button").click(function () {
-    $(".csr-intro").removeClass("has-left");
-    $(".csr-projects-section").removeClass("has-left");
-    $(".return-button").addClass("go-down");
-    $([document.documentElement, document.body]).animate(
-      {
-        scrollTop: $(".csr-mid").offset().top - 30,
-      },
-      600
-    );
-  });
+		$('.return-button').removeClass('go-down');
+	});
+
+	//   CLICK RETURN
+	$('.return-button').click(function () {
+		$('.csr-intro').removeClass('has-left');
+		$('.csr-projects-section').removeClass('has-left');
+		$('.return-button').addClass('go-down');
+		$([document.documentElement, document.body]).animate(
+			{
+				scrollTop: $('.csr-mid').offset().top - 30,
+			},
+			600
+		);
+	});
 });
